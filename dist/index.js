6a21a4879e2223e4326d80c399cf42db8186cef0
BenC0 <suddensquirrel94@gmail.com>
2021-06-01 16:43:50 +0100

    Built dist

--- dist/index.js
+++ dist/index.js
@@ -53,7 +53,7 @@ function set(cname, cvalue) {
 }
 /* function to retrieve a cookie value */
 
-function get() {
+function get$1() {
   var cname = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
 
   if (cname != false) {
@@ -78,7 +78,7 @@ function get() {
 }
 /* Function to check a cookie exists */
 
-function exists() {
+function exists$1() {
   var cookieName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
 
   /* If cookieName is not false and exists */
@@ -91,27 +91,105 @@ function exists() {
 }
 var cookieFunctions = {
   set: set,
-  get: get,
-  exists: exists
+  get: get$1,
+  exists: exists$1
 };
 
 function registerTest(testID, variant, extraDetails) {
-  window.norman = window.norman || {
-    isPDP: isPDP(),
-    isPLP: isPLP(),
-    isHome: isHome(),
-    pageType: pageType()
-  };
-  window.norman[testID] = {
-    logs: [],
+  var testConfig = {
     variant: variant,
-    testID: testID
+    id: testID
   };
 
   for (var property in extraDetails) {
-    window.norman[testID][property] = extraDetails[property];
+    testConfig[property] = extraDetails[property];
   }
+
+  window.norman = window.norman || [];
+  window.norman[testID] = {
+    logs: [],
+    testConfig: testConfig
+  };
+  return window.norman[testID];
 }
+
+function _toConsumableArray(arr) {
+  return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread();
+}
+
+function _arrayWithoutHoles(arr) {
+  if (Array.isArray(arr)) return _arrayLikeToArray(arr);
+}
+
+function _iterableToArray(iter) {
+  if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);
+}
+
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+  return arr2;
+}
+
+function _nonIterableSpread() {
+  throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+
+function get(selector) {
+  var all = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+  // Get element
+  var els = [];
+
+  if (all && typeof selector === "string" && selector.replace(/ /g, '') !== "") {
+    els = _toConsumableArray(document.querySelectorAll(selector));
+  } else {
+    els = [document.querySelector(selector)];
+  }
+
+  return els.length !== 0 ? els : false;
+}
+function exists(input) {
+  // Check element exists
+  if (typeof input === "string") {
+    return !!document.querySelector(input);
+  }
+
+  return !!input;
+}
+function add(html, target, method) {
+  // add element to page
+  var template = document.createElement('template');
+  html = html.trim();
+  template.innerHTML = html;
+  var tempEl = template.content.firstChild;
+  var targetEl = document.querySelector(target);
+  return targetEl.insertAdjacentElement("beforebegin", tempEl);
+}
+function remove(input) {
+  // Remove element
+  if (typeof input === "string") {
+    input = document.querySelector(input);
+  }
+
+  input.remove();
+}
+var elementManagement = {
+  get: get,
+  exists: exists,
+  add: add,
+  remove: remove
+};
 
 // Returns a function, that, as long as it continues to be invoked, will not
 // be triggered. The function will be called after it stops being called for
@@ -222,6 +300,7 @@ function getHighestZIndex() {
 
 exports.cookie = cookieFunctions;
 exports.debounce = debounce;
+exports.elementManagement = elementManagement;
 exports.getHighestZIndex = getHighestZIndex;
 exports.isInViewport = isInViewport;
 exports.log = log;
